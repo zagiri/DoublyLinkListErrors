f97ddf2b0956418b58cf01043037e0d2666a6740
zagir <zagiri.bot@gmail.com>
2023-06-13 20:16:03 +1200

    Fixed Errors in all code

--- DoublyLinkedListWithErrors/Program.cs
+++ DoublyLinkedListWithErrors/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,11 @@ namespace DoublyLinkedListWithErrors
 {
     class Program
     {
+        [ExcludeFromCodeCoverage]
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the doubly linked list with errors");
+           Console.ReadLine();
             return; // return
         }
     }
